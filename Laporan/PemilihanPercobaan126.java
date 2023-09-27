bbd5d3438b61249f4fddbdab9ef703503c61c100
5atriy0 <143465198+5atriy0@users.noreply.github.com>
2023-09-27 14:02:44 +0700

    Add files via upload

---
+++ Laporan/PemilihanPercobaan126.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan126 {
+
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+        System.out.println("Masukkan angka: ");
+        int angka = input26.nextInt();
+        String hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+        System.out.println("Angka " + angka + " bilangan " + hasil);
+    }
+}
