7a0993d04478b7421fc15b449d59c4fed003a370
5atriy0 <satriyobagus47@gmail.com>
2023-10-04 10:42:15 +0700

    Percobaan3

--- Jobsheet6/Pemilihan2Percobaan326.java
+++ Jobsheet6/Pemilihan2Percobaan326.java
@@ -7,9 +7,28 @@ public class Pemilihan2Percobaan326 {
         int penghasilan, gajiBersih;
         double pajak = 0;
         System.out.print("Masukkan Kategori : ");
-        kategori = 26.nextLine();
+        kategori = input26.nextLine();
         System.out.print("Masukkan Besarnya Penghasilan : ");
-        penghasilan = 26.nextint();
-        
-    }
-}
+        penghasilan = input26.nextInt();
+        if (kategori.equals("pekerja")) {
+            if (penghasilan <= 2000000){
+                pajak = 0.1;
+            } else if (penghasilan <=3000000){
+                pajak = 0.15;
+            } else
+                pajak = 0.2;
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.print("Penghasil Bersih : " + gajiBersih);
+            } else if (kategori.equals("pebisnis")) {
+                if (penghasilan <= 2500000){
+                    pajak = 0.15;
+                } else if (penghasilan <= 3500000){
+                    pajak = 0.2;
+                } else
+                    pajak = 0.25;
+                gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+                System.out.print("Penghasil Bersih : " + gajiBersih);
+                } else
+                    System.out.println("Masukan Kategori Salah ");
+                }
+            }
