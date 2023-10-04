36f7b3eb1155205d682cb1b5b8db111c59f8aa59
5atriy0 <satriyobagus47@gmail.com>
2023-10-04 09:10:31 +0700

    p6

---
+++ Laporan/Jobsheet6/Pemilihan2Percobaan326.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan326 {
+
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+        String kategori;
+        int penghasilan, gajiBersih;
+        double pajak = 0;
+        System.out.print("Masukkan Kategori : ");
+        kategori = 26.nextLine();
+        System.out.print("Masukkan Besarnya Penghasilan : ");
+        penghasilan = 26.nextint();
+        
+    }
+}
