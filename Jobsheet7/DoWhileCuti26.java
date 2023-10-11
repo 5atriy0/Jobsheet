372d0e5b7076e63c727f38db61cb9c8b98be8c75
5atriy0 <satriyobagus47@gmail.com>
2023-10-11 09:32:48 +0700

    Jobsheet7

---
+++ Jobsheet7/DoWhileCuti26.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class DoWhileCuti26 {
+
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = input26.nextInt();
+
+        do {
+            System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = input26.next();
+
+            if (konfirmasi.equalsIgnoreCase("y"));
+                System.out.print("Jumlah hari: ");
+                jumlahHari = input26.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                }else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+            } while (jatahCuti > 0);
+        }
+    }
