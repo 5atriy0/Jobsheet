8439f4637067f36e26cc277373049c81bccc0a8e
5atriy0 <satriyobagus47@gmail.com>
2023-10-25 13:02:37 +0700

    Modifikasi

--- Jobsheet9/ArrayNilai26.java
+++ Jobsheet9/ArrayNilai26.java
@@ -3,12 +3,17 @@ public class ArrayNilai26 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = scan.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Masukkan nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+            System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }
+            else if (nilaiAkhir[i] < 70) {
+                System.out.println("Mahasiwa ke-"+i+" tidak lulus! ");
+            }
+        }
     }
-}
 }
