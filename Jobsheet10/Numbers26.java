7663fef610a407c3ab75d68490cba8bf3e8682f6
5atriy0 <satriyobagus47@gmail.com>
2023-11-01 11:27:55 +0700

    modified

--- Jobsheet10/Numbers26.java
+++ Jobsheet10/Numbers26.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Numbers26 {
 
     public static void main(String[] args) {
@@ -5,5 +7,12 @@ public class Numbers26 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang bari ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     }
 }
